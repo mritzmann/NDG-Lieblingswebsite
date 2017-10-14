7c4e709f75bf78bc355fc0014b8a3de326bb76aa
Markus Ritzmann <github@markus-mail.com>
2017-10-14 11:18:03 +0000

    Apply fixes from StyleCI

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
   // Nimmt eingabe entgegen und speichert in eine variable.
-  $domain = $_POST["eingabe"];
+  $domain = $_POST['eingabe'];
 
   // Bereinige Eingabe
-  $domain = htmlspecialchars($domain, ENT_QUOTES,'UTF-8', true);
+  $domain = htmlspecialchars($domain, ENT_QUOTES, 'UTF-8', true);
 
   // workaround damit api abfrage nur dann gemacht wird, wenn formular abgesendet wurde.
-  if( empty($_POST) ) {
-    // tut nix
+  if (empty($_POST)) {
+      // tut nix
   } else {
-    // entfernt http und https aus der eingabe
-    $domain = preg_replace('#^https?://#', '', $domain);
-    // Taetigt API abfrage und speichert resultat in eine variable.
-    // Die API ermoeglicht 150 anfragen pro Minute.
-    $json = file_get_contents('http://ip-api.com/json/'.$domain.'?fields=country&lang=de');
+      // entfernt http und https aus der eingabe
+      $domain = preg_replace('#^https?://#', '', $domain);
+      // Taetigt API abfrage und speichert resultat in eine variable.
+      // Die API ermoeglicht 150 anfragen pro Minute.
+      $json = file_get_contents('http://ip-api.com/json/'.$domain.'?fields=country&lang=de');
   }
 
   // keine ahnung was da genau passiert...
@@ -22,16 +22,16 @@
   $ausgabe = $obj->country;
 
   // kuemmert sich darum, dass das Formular auch nach dem absenden anzeigt was eingegeben wurde.
-  $placeholder = "www.blick.ch";
-  if( empty($_POST) ) {
-    // tut nix
+  $placeholder = 'www.blick.ch';
+  if (empty($_POST)) {
+      // tut nix
   } else {
-    // ueberschreibt placeholder
-    $placeholder = $domain;
+      // ueberschreibt placeholder
+      $placeholder = $domain;
   }
 
   // speichert die aktuelle url (aus der Browser adressleiste) in eine Variable.
-  $url = ((empty($_SERVER['HTTPS'])) ? 'http://' : 'https://') . $_SERVER['HTTP_HOST'];
+  $url = ((empty($_SERVER['HTTPS'])) ? 'http://' : 'https://').$_SERVER['HTTP_HOST'];
 ?>
 
 <!DOCTYPE html>
@@ -87,15 +87,14 @@
     <hr>
 
     <?php
-    if( empty($ausgabe) ) {
-      echo "<div class=\"panel panel-default\"> <div class=\"panel-heading\"> <h3 class=\"panel-title\">Technische Umsetzung</h3> </div> <div class=\"panel-body\">Diese Webseite prüft anhand einer Geo-IP API, in welchem Land die zu prüfende Webseite steht. Das Resultat wird Dir direkt mitgeteilt.</div> </div>";
+    if (empty($ausgabe)) {
+        echo '<div class="panel panel-default"> <div class="panel-heading"> <h3 class="panel-title">Technische Umsetzung</h3> </div> <div class="panel-body">Diese Webseite prüft anhand einer Geo-IP API, in welchem Land die zu prüfende Webseite steht. Das Resultat wird Dir direkt mitgeteilt.</div> </div>';
     } else {
-
-      if ($ausgabe == 'Schweiz') {
-          echo "<div class=\"panel panel-warning\" id=\"resultat\"> <div class=\"panel-heading\"> <h3 class=\"panel-title\"><span class=\"glyphicon glyphicon-ok\" aria-hidden=\"true\"></span>Glückwunsch, du wirst vielleicht nicht überwacht!</h3> </div> <div class=\"panel-body\">Die Webseite <a href=\"http://$domain\" target=\"_blank\"><b>$domain</b></a> wird in der <b>$ausgabe</b> gehostet. Es besteht die Möglichkeit, dass der Nachrichtendienst den Inhalt der Kommunikation nicht mitschneiden darf.</br></br>Behalte jedoch immer im Hinterkopf: Dank dem Überwachungsgesetz BÜPF hinterlässt Du dennoch Metadaten. Der Nachrichtendienst kann auf diese zugreifen. Zudem besteht die Möglichkeit, dass die Route ins Ausland und anschliessend wieder zurück in die Schweiz führt.</div> </div>";
-      } else {
-          echo "<div class=\"panel panel-danger\" id=\"resultat\"> <div class=\"panel-heading\"> <h3 class=\"panel-title\"><span class=\"glyphicon glyphicon-remove\" aria-hidden=\"true\"></span>Du wirst überwacht!</h3> </div> <div class=\"panel-body\">Die Webseite <a href=\"http://$domain\" target=\"_blank\"><b>$domain</b></a> wird in <b>$ausgabe</b> gehostet. Beim Aufrufen von $domain findet grenzüberschreitende Kommunikation statt. Dabei wirst Du überwacht!</br></br>Weitere Informationen:</br><ul><li><a href=\"https://www.nachrichtendienstgesetz.ch/#faq\" target=\"_blank\">5 Gründe gegen den Ausbau der Überwachung</a></li><li><a href=\"http://schnueffelstaat.ch/\" target=\"_blank\">Referendumskomitee: NEIN zum Schnüffelstaat</a></li><li><a href=\"https://www.nachrichtendienstgesetz.com/\" target=\"_blank\">Abhörsystem Codename ISCO</a></li></ul></div></div><div class=\"panel panel-default\"><div class=\"panel-heading\"><h3 class=\"panel-title\"><span class=\"glyphicon glyphicon-heart\" aria-hidden=\"true\"></span>Teile Dein Ergebnis</h3></div><div class=\"panel-body\">Teile Dein Ergebnis! Du hilfst damit, andere auf diesen Missstand aufmerksam zu machen.<hr><a type=\"button\" href=\"https://twitter.com/intent/tweet?url=$url&text=Meine%20Lieblingswebsite%20wird%20%C3%BCberwacht.%20Und%20deine%3F%20Finde%20es%20heraus%3A&original_referer=&via=ueberwacht_ch&hashtags=NDGNein%2C\" target=\"_blank\" class=\"btn btn-default\"><i class=\"fa fa-twitter\"></i> Twitter</a> <a type=\"button\" href=\"https://www.facebook.com/sharer/sharer.php?u=$url\" target=\"_blank\" class=\"btn btn-default\"><i class=\"fa fa-facebook\"></i> Facebook</a> <a type=\"button\" href=\"mailto:?&subject=Prüfe ob Du beim Besuch Deiner Lieblingswebsite überwacht wirst&body=Die%20sogenannte%20Kabelaufkl%C3%A4rung%20erm%C3%B6glicht%20es%2C%20dem%20Nachrichtendienst%20grenz%C3%BCberschreitende%20Kommunikation%20zu%20%C3%BCberwachen.%20Die%20Bef%C3%BCrworter%20behaupten%2C%20dass%20Schweizer%20somit%20nicht%20von%20der%20Massen%C3%BCberwachung%20betroffen%20seien.%20Doch%20das%20stimmt%20nicht%20ganz%3A%20Die%20meisten%20(vielleicht%20sogar%20alle)%20Deiner%20Lieblingswebsites%20werden%20im%20Ausland%20gehostet.%20Nachfolgend%20kannst%20Du%20pr%C3%BCfen%2C%20ob%20das%20stimmt.%0A%0A$url\" class=\"btn btn-default\"><i class=\"fa fa-envelope\"></i> E-Mail</a></div> </div>";
-          echo '
+        if ($ausgabe == 'Schweiz') {
+            echo "<div class=\"panel panel-warning\" id=\"resultat\"> <div class=\"panel-heading\"> <h3 class=\"panel-title\"><span class=\"glyphicon glyphicon-ok\" aria-hidden=\"true\"></span>Glückwunsch, du wirst vielleicht nicht überwacht!</h3> </div> <div class=\"panel-body\">Die Webseite <a href=\"http://$domain\" target=\"_blank\"><b>$domain</b></a> wird in der <b>$ausgabe</b> gehostet. Es besteht die Möglichkeit, dass der Nachrichtendienst den Inhalt der Kommunikation nicht mitschneiden darf.</br></br>Behalte jedoch immer im Hinterkopf: Dank dem Überwachungsgesetz BÜPF hinterlässt Du dennoch Metadaten. Der Nachrichtendienst kann auf diese zugreifen. Zudem besteht die Möglichkeit, dass die Route ins Ausland und anschliessend wieder zurück in die Schweiz führt.</div> </div>";
+        } else {
+            echo "<div class=\"panel panel-danger\" id=\"resultat\"> <div class=\"panel-heading\"> <h3 class=\"panel-title\"><span class=\"glyphicon glyphicon-remove\" aria-hidden=\"true\"></span>Du wirst überwacht!</h3> </div> <div class=\"panel-body\">Die Webseite <a href=\"http://$domain\" target=\"_blank\"><b>$domain</b></a> wird in <b>$ausgabe</b> gehostet. Beim Aufrufen von $domain findet grenzüberschreitende Kommunikation statt. Dabei wirst Du überwacht!</br></br>Weitere Informationen:</br><ul><li><a href=\"https://www.nachrichtendienstgesetz.ch/#faq\" target=\"_blank\">5 Gründe gegen den Ausbau der Überwachung</a></li><li><a href=\"http://schnueffelstaat.ch/\" target=\"_blank\">Referendumskomitee: NEIN zum Schnüffelstaat</a></li><li><a href=\"https://www.nachrichtendienstgesetz.com/\" target=\"_blank\">Abhörsystem Codename ISCO</a></li></ul></div></div><div class=\"panel panel-default\"><div class=\"panel-heading\"><h3 class=\"panel-title\"><span class=\"glyphicon glyphicon-heart\" aria-hidden=\"true\"></span>Teile Dein Ergebnis</h3></div><div class=\"panel-body\">Teile Dein Ergebnis! Du hilfst damit, andere auf diesen Missstand aufmerksam zu machen.<hr><a type=\"button\" href=\"https://twitter.com/intent/tweet?url=$url&text=Meine%20Lieblingswebsite%20wird%20%C3%BCberwacht.%20Und%20deine%3F%20Finde%20es%20heraus%3A&original_referer=&via=ueberwacht_ch&hashtags=NDGNein%2C\" target=\"_blank\" class=\"btn btn-default\"><i class=\"fa fa-twitter\"></i> Twitter</a> <a type=\"button\" href=\"https://www.facebook.com/sharer/sharer.php?u=$url\" target=\"_blank\" class=\"btn btn-default\"><i class=\"fa fa-facebook\"></i> Facebook</a> <a type=\"button\" href=\"mailto:?&subject=Prüfe ob Du beim Besuch Deiner Lieblingswebsite überwacht wirst&body=Die%20sogenannte%20Kabelaufkl%C3%A4rung%20erm%C3%B6glicht%20es%2C%20dem%20Nachrichtendienst%20grenz%C3%BCberschreitende%20Kommunikation%20zu%20%C3%BCberwachen.%20Die%20Bef%C3%BCrworter%20behaupten%2C%20dass%20Schweizer%20somit%20nicht%20von%20der%20Massen%C3%BCberwachung%20betroffen%20seien.%20Doch%20das%20stimmt%20nicht%20ganz%3A%20Die%20meisten%20(vielleicht%20sogar%20alle)%20Deiner%20Lieblingswebsites%20werden%20im%20Ausland%20gehostet.%20Nachfolgend%20kannst%20Du%20pr%C3%BCfen%2C%20ob%20das%20stimmt.%0A%0A$url\" class=\"btn btn-default\"><i class=\"fa fa-envelope\"></i> E-Mail</a></div> </div>";
+            echo '
           <!-- Piwik -->
           <script type="text/javascript">
             var _paq = _paq || [];
@@ -113,7 +112,7 @@
           <noscript><p><img src="//stat.ueberwacht.ch/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
           <!-- End Piwik Code -->
           ';
-      }
+        }
     }
     ?>
 
